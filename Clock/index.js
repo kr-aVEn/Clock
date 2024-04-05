5a6ff1e539792a552b22dfee2de2f8097a367ee1
Tharun <150697554+kr-aVEn@users.noreply.github.com>
2024-04-05 11:29:40 +0530

    Update index.js

--- Clock/index.js
+++ Clock/index.js
@@ -5,20 +5,23 @@ function clock(){
     let sec=time.getSeconds();
     const week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const date=week[time.getDate()];
-    let section=document.getElementById("day");
+
+    let section=document.getElementById("session");
     if(hrs>12){
         hrs=hrs-12;
     }
     if(hrs<10){
         hrs="0"+hrs;
     }
+    
     if(min<10){
         min="0"+min;
-
     }
+    
     if(sec<10){
-        sec="0"+sec;
+        sec="0"+sec; 
     }
+    
     if(hrs>=12){
         section.innerHTML="PM";
     }
@@ -26,7 +29,11 @@ function clock(){
         section.innerHTML="AM";
     }
     document.getElementById("hours").innerHTML=hrs;
-    document.getElementById("minute").innerHTML=hrs;
-    document.getElementById("hours").innerHTML=hrs;
-    
+    document.getElementById("minutes").innerHTML=min;
+    document.getElementById("seconds").innerHTML=sec;
+     document.getElementById("day") .innerHTML=date;  
 }
+
+setInterval(() => {
+    clock()
+}, 10);
